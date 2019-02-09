26a28745c97e44be893282a7fc850d8cc81b5e69
Josh Morrow <josh@jcmorrow.com>
2019-02-09 16:13:43 -0500

    Initial commit

---
+++ src/point/mod.rs
@@ -0,0 +1,367 @@
+pub struct Point {
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+}
+
+fn point() -> Point {
+    return Point {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+        w: 1.0,
+    };
+}
+
+fn vector() -> Point {
+    return Point {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+        w: 0.0,
+    };
+}
+
+impl Point {
+    fn is_point(&self) -> bool {
+        return equal(self.w, 1.0);
+    }
+
+    fn is_vector(&self) -> bool {
+        return equal(self.w, 0.0);
+    }
+
+    fn equal(&self, other: &Point) -> bool {
+        return equal(self.x, other.x)
+            && equal(self.y, other.y)
+            && equal(self.z, other.z)
+            && equal(self.w, other.w);
+    }
+
+    fn add(&self, other: &Point) -> Point {
+        return Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        };
+    }
+
+    fn sub(&self, other: &Point) -> Point {
+        return Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        };
+    }
+
+    fn multiply_scalar(&self, other: f64) -> Point {
+        return Point {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+            w: self.w * other,
+        };
+    }
+
+    fn divide_scalar(&self, other: f64) -> Point {
+        return Point {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+            w: self.w / other,
+        };
+    }
+
+    fn negative(&self) -> Point {
+        return vector().sub(&self);
+    }
+
+    fn magnitude(&self) -> f64 {
+        return (self.x.powi(2) + self.y.powi(2) + self.z.powi(2) + self.w.powi(2)).sqrt();
+    }
+
+    fn normalize(&self) -> Point {
+        return self.divide_scalar(self.magnitude());
+    }
+
+    fn dot(&self, other: &Point) -> f64 {
+        return self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w;
+    }
+}
+
+const EPSILON: f64 = 0.00001;
+fn equal(a: f64, b: f64) -> bool {
+    return (a - b).abs() < EPSILON;
+}
+
+#[cfg(test)]
+mod tests {
+    use point::equal;
+    use point::point;
+    use point::vector;
+    use point::Point;
+
+    #[test]
+    fn test_point() {
+        let a: Point = Point {
+            x: 4.3,
+            y: -4.2,
+            z: 3.1,
+            w: 1.0,
+        };
+
+        assert!(equal(a.x, 4.3));
+        assert!(equal(a.y, -4.2));
+        assert!(equal(a.z, 3.1));
+        assert!(equal(a.w, 1.0));
+        assert!(a.is_point());
+        assert!(!a.is_vector());
+    }
+
+    #[test]
+    fn test_vector() {
+        let a: Point = Point {
+            x: 4.3,
+            y: -4.2,
+            z: 3.1,
+            w: 0.0,
+        };
+
+        assert!(equal(a.x, 4.3));
+        assert!(equal(a.y, -4.2));
+        assert!(equal(a.z, 3.1));
+        assert!(equal(a.w, 0.0));
+        assert!(!a.is_point());
+        assert!(a.is_vector());
+    }
+
+    #[test]
+    fn test_new_point() {
+        let a: Point = point();
+
+        assert!(equal(a.x, 0.0));
+        assert!(equal(a.y, 0.0));
+        assert!(equal(a.z, 0.0));
+        assert!(equal(a.w, 1.0));
+        assert!(a.is_point());
+        assert!(!a.is_vector());
+    }
+
+    #[test]
+    fn test_new_vector() {
+        let a: Point = vector();
+
+        assert!(equal(a.x, 0.0));
+        assert!(equal(a.y, 0.0));
+        assert!(equal(a.z, 0.0));
+        assert!(equal(a.w, 0.0));
+        assert!(!a.is_point());
+        assert!(a.is_vector());
+    }
+
+    #[test]
+    fn test_vector_equality() {
+        let a: Point = vector();
+        let b: Point = vector();
+
+        assert!(a.equal(&b));
+    }
+
+    #[test]
+    fn test_point_equality() {
+        let a: Point = point();
+        let b: Point = point();
+
+        assert!(a.equal(&b));
+    }
+
+    #[test]
+    fn test_add_vector_to_vector() {
+        let a: Point = Point {
+            x: 3.0,
+            y: -2.0,
+            z: 5.0,
+            w: 0.0,
+        };
+        let b: Point = Point {
+            x: -2.0,
+            y: 3.0,
+            z: 1.0,
+            w: 0.0,
+        };
+        let sum = a.add(&b);
+
+        assert!(equal(sum.x, 1.0));
+        assert!(equal(sum.y, 1.0));
+        assert!(equal(sum.z, 6.0));
+        assert!(equal(sum.w, 0.0));
+    }
+
+    #[test]
+    fn test_add_vector_to_point() {
+        let a: Point = Point {
+            x: 3.0,
+            y: -2.0,
+            z: 5.0,
+            w: 0.0,
+        };
+        let b: Point = Point {
+            x: -2.0,
+            y: 3.0,
+            z: 1.0,
+            w: 1.0,
+        };
+        let sum = a.add(&b);
+
+        assert!(equal(sum.x, 1.0));
+        assert!(equal(sum.y, 1.0));
+        assert!(equal(sum.z, 6.0));
+        assert!(equal(sum.w, 1.0));
+    }
+
+    #[test]
+    fn test_subtract_point_from_point() {
+        let a: Point = Point {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+            w: 1.0,
+        };
+        let b: Point = Point {
+            x: 5.0,
+            y: 6.0,
+            z: 7.0,
+            w: 1.0,
+        };
+        let diff = a.sub(&b);
+
+        assert!(equal(diff.x, -2.0));
+        assert!(equal(diff.y, -4.0));
+        assert!(equal(diff.z, -6.0));
+        assert!(equal(diff.w, 0.0));
+    }
+
+    #[test]
+    fn test_subtract_vector_from_point() {
+        let a: Point = Point {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+            w: 1.0,
+        };
+        let b: Point = Point {
+            x: 5.0,
+            y: 6.0,
+            z: 7.0,
+            w: 0.0,
+        };
+        let diff = a.sub(&b);
+
+        assert!(equal(diff.x, -2.0));
+        assert!(equal(diff.y, -4.0));
+        assert!(equal(diff.z, -6.0));
+        assert!(equal(diff.w, 1.0));
+    }
+
+    #[test]
+    fn test_negative() {
+        let vector = Point {
+            x: 1.0,
+            y: -2.0,
+            z: 3.0,
+            w: 0.0,
+        };
+        assert!(vector.negative().equal(&Point {
+            x: -1.0,
+            y: 2.0,
+            z: -3.0,
+            w: 0.0,
+        }))
+    }
+
+    #[test]
+    fn test_vector_multiply_scalar() {
+        let a: Point = Point {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+            w: 0.0,
+        };
+        let product = a.multiply_scalar(2.0);
+
+        assert!(equal(product.x, 6.0));
+        assert!(equal(product.y, 4.0));
+        assert!(equal(product.z, 2.0));
+        assert!(equal(product.w, 0.0));
+    }
+
+    #[test]
+    fn test_vector_divide_scalar() {
+        let a: Point = Point {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+            w: 0.0,
+        };
+        let product = a.divide_scalar(2.0);
+
+        assert!(equal(product.x, 1.5));
+        assert!(equal(product.y, 1.0));
+        assert!(equal(product.z, 0.5));
+        assert!(equal(product.w, 0.0));
+    }
+
+    #[test]
+    fn test_magnitude() {
+        let a: Point = Point {
+            x: -1.0,
+            y: -2.0,
+            z: -3.0,
+            w: 0.0,
+        };
+
+        const FOURTEEN: f64 = 14.0;
+
+        assert!(equal(a.magnitude(), FOURTEEN.sqrt()));
+    }
+
+    #[test]
+    fn test_normalize() {
+        let a: Point = Point {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+            w: 0.0,
+        };
+
+        const FOURTEEN: f64 = 14.0;
+
+        assert!(a.normalize().equal(&Point {
+            x: 1.0 / FOURTEEN.sqrt(),
+            y: 2.0 / FOURTEEN.sqrt(),
+            z: 3.0 / FOURTEEN.sqrt(),
+            w: 0.0
+        }));
+    }
+
+    #[test]
+    fn test_dot_product() {
+        let a: Point = Point {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+            w: 0.0,
+        };
+        let b: Point = Point {
+            x: 2.0,
+            y: 3.0,
+            z: 4.0,
+            w: 0.0,
+        };
+
+        assert!(equal(a.dot(&b), 20.0));
+    }
+}
---
+++ src/vector.rs
@@ -0,0 +1,6 @@
+pub struct Point {
+    x: i64,
+    y: i64,
+    z: i64,
+    w: i64,
+}
